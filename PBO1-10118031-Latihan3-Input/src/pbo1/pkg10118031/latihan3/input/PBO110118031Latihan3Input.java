08276cb88a5029533186cf89872403e9d6a586f9
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 11:52:14 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan3-Input/src/pbo1/pkg10118031/latihan3/input/PBO110118031Latihan3Input.java
@@ -0,0 +1,24 @@
+package pbo1.pkg10118031.latihan3.input;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vendetta NAMA : RIZKI ARIA PUTRA 
+ * KELAS                 : PBO1 
+ * NIM                   : 10118031
+ * Deskripsi Program     : Program ini berisi program untuk menginput
+ */
+
+public class PBO110118031Latihan3Input {
+
+    public static void main(String[] args) {
+        System.out.println("Masukan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " + nama);
+
+    }
+
+}
